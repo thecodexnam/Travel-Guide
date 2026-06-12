4f1bb6f161b44b61d22b3b2afea27cf91cdbbf67
namanydv <namanofficial91@gmail.com>
2026-06-12 17:11:40 +0530

    Remove background color from RootLayout body class

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${boldonse.className} min-h-screen bg-black pt-28 text-white md:pt-24`}
+        className={`${boldonse.className} min-h-screen pt-28 text-white md:pt-24`}
       >
         <NavBar />
         {children}
